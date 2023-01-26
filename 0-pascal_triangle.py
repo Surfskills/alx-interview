2c7b5dc3d7279fd6c28f90065d29f0f4131fdb61
Surfskills <ojjfred@gmail.com>
2023-01-26 20:08:31 +0300

    Pascal's triangle

--- 0-pascal_triangle.py
+++ 0-pascal_triangle.py
@@ -1,38 +1,10 @@
-# Python 3 code for Pascal's Triangle
-# A simple O(n^3)
-# program for
-# Pascal's Triangle
-
-# Function to print
-# first n lines of
-# Pascal's Triangle
-def printPascal(n) :
-	
-	# Iterate through every line
-	# and print entries in it
-	for line in range(0, n) :
-		
-		# Every line has number of
-		# integers equal to line
-		# number
-		for i in range(0, line + 1) :
-			print(binomialCoeff(line, i),
-				" ", end = "")
-		print()
-	
-def binomialCoeff(n, k) :
-	res = 1
-	if (k > n - k) :
-		k = n - k
-	for i in range(0 , k) :
-		res = res * (n - i)
-		res = res // (i + 1)
-	
-	return res
-
-# Driver program
-n = 7
-printPascal(n)
-
-
-#Code contributions from Geeks @fred
+def pascalsTriangle():
+  height = int(input("Enter height of triangle: "))
+  triangle = []
+  row = []
+  prev_row = []
+  for i in xrange(0, height + 1):
+    row = [j > 0 and j < i - 1 and i > 2 and prev_row[j-1] + prev_row[j] or 1 for j in xrange(0, i)]
+    prev_row = row
+    triangle += [row]
+  return triangle[1:]
