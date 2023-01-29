4483d18ce13d4bc328e8815c05c210cbccf0b490
Surfskills <ojjfred@gmail.com>
2023-01-29 13:15:39 -0800

    Interview

--- 0-pascal_triangle.py
+++ 0-pascal_triangle.py
@@ -1,12 +1,18 @@
-#!/usr/bin/python3
+def pascal_triangle(n):                                                                                                               
 
-def pascalsTriangle():
-  height = int(input("Enter height of triangle: "))
-  triangle = []
-  row = []
-  prev_row = []
-  for i in xrange(0, height + 1):
-    row = [j > 0 and j < i - 1 and i > 2 and prev_row[j-1] + prev_row[j] or 1 for j in xrange(0, i)]
-    prev_row = row
-    triangle += [row]
-  return triangle[1:]
+if n <= 0:                                                                                                                            
+
+return []                                                                                                                             
+
+triangle = [[1]]                                                                                                                      
+
+for i in range(1, n):                                                                                                                 
+
+row = [1]                                                                                                                             
+
+for j in range(1, i):                                                                                                                 
+
+row.append(triangle[i-1][j-1] + triangle[i-1][j])                                                                                     
+
+row.append(1) triangle.append(row)                                                                                                    
+
