305ba44edffdee2b51432a99e40034825656b2f1
Surfskills <ojjfred@gmail.com>
2023-01-29 15:41:28 -0800

    Interview

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,133 +1,5 @@
 #!/usr/bin/python3
 
-
-
-"""
-
-Defines function that returns a list of lists of integers
-
-representing the Pascal's triangle of n
-
-"""
-
-
-
-
-
-
-
-
-
-
-
-def pascal_triangle(n):
-
-
-
-    """
-
-    Creates a list of lists of integers representing Pascal's triangle
-
-    parameters:
-
-        n [int]:
-
-            the number of rows of Pascal's triangle to recreate
-
-    return:
-
-        [list of lists of ints]:
-
-            representation of Pascal's triangle
-
-    """
-
-
-
-    if type(n) is not int:
-
-
-
-        raise TypeError("n must be an integer")
-
-
-
-    triangle = []
-
-
-
-    if n <= 0:
-
-
-
-        return triangle
-
-
-
-    previous = [1]
-
-
-
-    for row_index in range(n):
-
-
-
-        rowlist = []
-
-
-
-        if row_index == 0:
-
-
-
-            rowlist = [1]
-
-
-
-        else:
-
-
-
-            for i in range(row_index + 1):
-
-
-
-                if i == 0:
-
-
-
-                    rowlist.append(0 + previous[i])
-
-
-
-                elif i == (row_index):
-
-
-
-                    rowlist.append(previous[i - 1] + 0)
-
-
-
-                else:
-
-
-
-                    rowlist.append(previous[i - 1] + previous[i])
-
-
-
-        previous = rowlist
-
-
-
-        triangle.append(rowlist)
-
-
-
-#!/usr/bin/python3
-
-
-
 """
 
 Pascal's Triangle
@@ -138,43 +10,21 @@ Pascal's Triangle
 
 
 
-
-
-
-
-
-
 def binomialCoeff(n, k):
-
-
 
     """ calculate the value of Binomial Coefficient """
 
-
-
     res = 1
-
-
 
     if (k > n - k):
 
-
-
         k = n - k
-
-
 
     for i in range(0, k):
 
-
-
         res = res * (n - i)
 
-
-
         res = res // (i + 1)
-
-
 
     return res
 
@@ -182,15 +32,7 @@ def binomialCoeff(n, k):
 
 
 
-
-
-
-
-
-
 def pascal_triangle(n):
-
-
 
     """
 
@@ -202,29 +44,15 @@ def pascal_triangle(n):
 
     """
 
-
-
     l1 = []
-
-
 
     for line in range(0, n):
 
-
-
         l2 = []
-
-
 
         for i in range(0, line + 1):
 
-
-
             l2.append(binomialCoeff(line, i))
 
-
-
         l1.append(l2)
-
-
 
